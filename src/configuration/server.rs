2a63331fa5567855e9744e81a6f76e5d0a47a4e0
bogdanmcs <bogdansadovan@yahoo.com>
2024-09-11 00:11:49 +0300

    Migrate web framework from Tide to Rocket

--- src/configuration/server.rs
+++ src/configuration/server.rs
@@ -1,17 +1,39 @@
-use crate::controller::crud_controller;
+use rocket::{figment::Figment, Config};
+
+use crate::controller::rocket_controller;
 
 pub struct Server {}
 
 impl Server {
-    pub async fn new(host: Box<str>, port: Box<str>) -> tide::Result<()> {
 
-        let app = crud_controller::set_urls().await;
+    pub async fn new(host: Box<str>, port: Box<str>) -> Result<(), rocket::Error> {
+        let figment: Figment = Server::config(host, port);
+        let routes: Vec<rocket::Route> = Server::routes();
 
-        app.listen(host.to_string() + ":" + &*port).await?;
+        let _rocket: rocket::Rocket<rocket::Ignite> = rocket::custom(figment)
+        .mount("/", routes)
+        .launch()
+        .await?;
 
         Ok(())
-
     }
 
+    fn config(host: Box<str>, port: Box<str>) -> Figment {
+        let port: i32 = port.parse().expect("The provided port is not a valid integer.");
+
+        let figment: Figment = Config::figment()
+        .merge(("address", host.to_string()))
+        .merge(("port", port));
+
+        figment
+    }
+
+    fn routes() -> Vec<rocket::Route> {
+        let mut routes: Vec<rocket::Route> = vec![];
+    
+        routes.extend(rocket_controller::routes());
+    
+        routes
+    }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,9 @@ mod model;
 use server_proxy::configuration::parser::Parser;
 use crate::configuration::server;
 
-#[async_std::main]
-async fn main() -> tide::Result<()> {
- Parser::read_configuration();
- server::Server::new(Box::from("0.0.0.0"), Box::from("8080")).await
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    Parser::read_configuration();
+    server::Server::new(Box::from("0.0.0.0"), Box::from("8080")).await
 }
-
-
